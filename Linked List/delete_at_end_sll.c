9d56fd64f0dc8dcb6717ae52132fe416e6c19cfd
Ujjwal Dash <ujjwaldash2007@gmail.com>
2021-12-30 19:01:51 +0530

    added delete_at_end_sll

---
+++ Linked List/delete_at_end_sll.c
@@ -0,0 +1,71 @@
+
+//deletioon at end
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+	int data;
+	struct node *next;
+};
+
+struct node *head=0;
+
+void create();
+void show();
+void delete_at_end();
+
+void main()
+{
+	int i=0;
+	for(i=0;i<5;i++)
+	{
+		create();
+	}
+	show();
+	delete_at_end();
+	show();
+}
+void create()
+{
+	struct node *newnode,*temp;
+	newnode=(struct node *)malloc(sizeof(struct node));
+	printf("Enter Data:");
+	scanf("%d",&newnode->data);
+	newnode->next=0;
+	if(head==0)
+	{
+		head=temp=newnode;
+	}
+	else
+	{
+		temp->next=newnode;
+		temp=newnode;
+	}
+}
+void show()
+{
+	struct node *temp;
+	temp=head;
+	printf("| %d |",head);
+	while(temp!=0)
+	{
+		printf(" -> | %d | %d |",temp->data,temp->next);
+		temp=temp->next;
+	}
+	printf("\n");
+}
+void delete_at_end()
+{
+	struct node *temp,*prevnode;
+	temp=head;
+	while(temp->next!=0)
+	{
+		prevnode=temp;
+		temp=temp->next;
+	}
+	prevnode->next=0;
+	free(temp);
+}
+
